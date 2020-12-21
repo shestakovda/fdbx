5cb263bc6c64509a31fdf8aea3cf24e475f60948
Шестаков Денис <shestakovda@gmail.com>
2020-12-21 23:02:13 +0300

    fix: key copy

--- v2/key_fdb.go
+++ v2/key_fdb.go
@@ -11,28 +11,29 @@ func (k fdbKey) Printable() string { return fdb.Key(k).String() }
 
 func (k fdbKey) LSkip(i uint16) Key {
 	if i >= uint16(len(k)) {
-		k = nil
-	} else {
-		k = k[i:]
+		return Bytes2Key(nil)
 	}
-	return k
+
+	return Bytes2Key(k[i:])
 }
 
 func (k fdbKey) RSkip(i uint16) Key {
 	if i >= uint16(len(k)) {
-		k = nil
-	} else {
-		k = k[:len(k)-int(i)]
+		return Bytes2Key(nil)
 	}
-	return k
+
+	return Bytes2Key(k[:len(k)-int(i)])
 }
 
 func (k fdbKey) LPart(part ...byte) Key {
-	return fdbKey(append(part, k...))
+	return Bytes2Key(part).RPart(k...)
 }
 
 func (k fdbKey) RPart(part ...byte) Key {
-	return append(k, part...)
+	k2 := make(fdbKey, len(k)+len(part))
+	copy(k2[:len(k)], k)
+	copy(k2[len(k):], part)
+	return k2
 }
 
 func (k fdbKey) Clone() Key {
